0f1b4e96b8f8dab3fce3c374a4c03c36b3895eec
compylme <adthompson10@gmail.com>
2026-06-10 22:57:39 +0100

    add new routes to events

--- app/routes/events.py
+++ app/routes/events.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
+from uuid import UUID
 
-from app.schemas.events_types import EventCreate, EventResponse, OrganiserCreate, OrganiserResponse
+from app.schemas.events_types import EventCreate, EventResponse, OrganiserCreate, OrganiserResponse, EventUpdate, TicketTypesResponse
 from app.database import get_db
-from app.services.events import create_event, create_organisation
+from app.services.events_service import create_event, get_event, get_event_ticket_types, get_events, update_event, create_organisation
 
 router = APIRouter(prefix="/events", tags=["events"])
 
@@ -15,6 +16,39 @@ def post_organiser(payload: OrganiserCreate, db: Session = Depends(get_db)):
         email=payload.email,
     )
 
+@router.patch("/{event_id}", response_model=EventResponse)
+def patch_event(
+    event_id: UUID,
+    payload: EventUpdate, 
+    db: Session = Depends(get_db)
+):
+    return update_event(
+        db=db,
+        event_id=event_id,
+        update_data=payload
+    )
+
+@router.get("/{event_id}", response_model=EventResponse)
+def fetch_event(
+    event_id:UUID,
+    db: Session = Depends(get_db)
+): return get_event(
+    db=db,
+    event_id=event_id
+)
+
+@router.get("/{event_id}/ticket-types", response_model=TicketTypesResponse)
+def fetch_ticket_types(
+    event_id:UUID,
+    db: Session=Depends(get_db)):
+        return get_event_ticket_types(
+            db=db, event_id=event_id
+        )
+
+
+@router.get("/", response_model=list[EventResponse])
+def fetch_events(db: Session=Depends(get_db)):
+    return get_events(db)
 
 @router.post("/", response_model=EventResponse)
 def post_event(payload: EventCreate, db: Session = Depends(get_db)):
