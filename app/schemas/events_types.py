765e5d1b8404b3c8bf010491c0813f4423659a6e
compylme <adthompson10@gmail.com>
2026-06-10 22:58:38 +0100

    add ticketTypeResponse to event_types

--- app/schemas/events_types.py
+++ app/schemas/events_types.py
@@ -6,11 +6,26 @@ class TicketTypeCreate(BaseModel):
     price: float
     quantity_total: int
 
+class TicketTypeResponse(BaseModel):
+    id: UUID
+    name: str
+    price: float
+    quantity_total: int
+    quantity_sold: int
+   
+    model_config = {"from_attributes": True}
+
 class EventCreate(BaseModel):
     organiser_id: UUID
     name: str
     venue: str
     ticket_types: list[TicketTypeCreate]
+
+class EventUpdate(BaseModel):
+    name: str | None = None
+    venue: str | None = None
+    status: str | None = None
+    ticket_types: list[TicketTypeCreate] | None = None
 
 class EventResponse(BaseModel):
     id: UUID
@@ -19,6 +34,9 @@ class EventResponse(BaseModel):
     status: str
 
     model_config = {"from_attributes": True}
+
+class TicketTypesResponse(BaseModel):
+    ticket_types: list[TicketTypeResponse] | None
 
 class OrganiserCreate(BaseModel):
     name: str
