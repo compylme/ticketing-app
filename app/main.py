f711708f7556e18dfc8ff750df7c378131ddf238
compylme <adthompson10@gmail.com>
2026-06-12 23:46:46 +0100

    create user service and route

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from app.routes.events import router as events_router
+from app.routes.user import router as user_router
 
 app = FastAPI()
 
 app.include_router(events_router)
+app.include_router(user_router)
