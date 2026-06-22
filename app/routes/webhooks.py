08c935f1f09c71c4f500070712abce36aa7891ba
compylme <adthompson10@gmail.com>
2026-06-23 00:29:56 +0100

    webhook

--- app/routes/webhooks.py
+++ app/routes/webhooks.py
@@ -1,12 +1,14 @@
+import os
 import stripe
-from fastapi import FastAPI, Request, HTTPException
+from fastapi import Request, HTTPException, APIRouter
 
-app = FastAPI()
+stripe.api_key = os.getenv("STRIPE_SECRET_KEY")
+endpoint_secret = os.getenv("STRIPE_WEBHOOK_ENDPOINT_SECRET")
 
-stripe.api_key = "sk_test_xxx"
-endpoint_secret = "whsec_xxx"
+router = APIRouter(prefix="/tickets", tags=["tickets"])
 
-@app.post("/webhooks/stripe")
+
+@router.post("/webhooks/stripe")
 async def stripe_webhook(request: Request):
     payload = await request.body()
     sig_header = request.headers.get("stripe-signature")
@@ -20,10 +22,13 @@ async def stripe_webhook(request: Request):
     except stripe.SignatureVerificationError:
         raise HTTPException(status_code=400, detail="Invalud signature")
 
-    if event["type"] == "payment_intent.succeded":
+    if event["type"] == "payment_intent.succeeded":
         payment_intent = event["data"]["object"]
 
-        order_id = payment_intent["metadata"].get("order_id")
+        try:
+            order_id = payment_intent["metadata"]["order_id"]
+        except KeyError:
+            order_id = None
         payment_id = payment_intent["id"]
 
         print(f'payment has been made for {order_id} and this is the payment_id: {payment_id}')
